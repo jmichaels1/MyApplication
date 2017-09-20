36f05b0b0fdc1a10431c865f2a60cf2c8ae084e9
iam4208017 <michael.salr1590@gmail.com>
2017-09-20 20:07:37 +0200

    fyi

--- app/src/main/java/jmendez/escoladeltreball/org/myapplication/MainActivity.java
+++ app/src/main/java/jmendez/escoladeltreball/org/myapplication/MainActivity.java
@@ -2,6 +2,8 @@ package jmendez.escoladeltreball.org.myapplication;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     /**
@@ -12,5 +14,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void btn_clickme(View view){
+        Toast.makeText(this, "Hola, has dado click en Aceptar!", Toast.LENGTH_LONG).show();
     }
 }
